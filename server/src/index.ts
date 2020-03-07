5efd44b808dcbcea299ff1b4207ea9ca5dd638ce
Baha <ferchichibaha7@gmail.com>
2020-03-07 07:35:23 +0100

    fix the role based auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,40 @@
+import { Role } from './entity/Role';
+import { User } from './entity/User';
+import "dotenv/config"
 import { createConnection } from 'typeorm';
 import { UserResolver } from './UserResolver';
 import "reflect-metadata";
 import express from "express"
 import {ApolloServer} from "apollo-server-express"
 import { buildSchema } from "type-graphql";
+import { RoleEnum } from './RoleEnum';
 
 
 
 
 (async()=>{
     const app=express();
-    app.get('/',(_req,res)=>{
-        res.send("hello")
+    app.get('/',async(_req,res)=>{
+      const users=  await User.find()
+        res.json(users)
     })
-
     await createConnection()
+
+    for(let role in RoleEnum){
+       let r= Role.create({name:RoleEnum[role]})
+let rolename=await Role.findOne(r);
+        if(!rolename)
+        Role.insert({name:RoleEnum[role]})
+    }
+  
+
+
 
 const apolloserver =new ApolloServer({
 schema:await buildSchema({
     resolvers:[UserResolver]
-})
+}),
+context: ({req,res})=>({req,res})
 })
 
    apolloserver.applyMiddleware({app})
