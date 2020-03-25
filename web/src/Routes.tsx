faed97ab417aa9fa432498c8d1cbdcab4a435a8d
ferchichi baha <ferchichibaha7@gmail.com>
2020-03-25 13:21:54 +0100

    yo 1

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -22,6 +22,8 @@ return <BrowserRouter>
 <switch>
 <Route exact path="/" component={Home}/>
 <Route exact path="/" component={Home}/>
+<Route exact path="/" component={Home}/>
+<Route exact path="/login" component={Login}/>
 <Route exact path="/login" component={Login}/>
 <Route exact path="/register" component={Register}/>
 <Route exact path="/profile" component={Profile}/>
